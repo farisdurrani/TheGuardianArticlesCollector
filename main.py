14de32b128941865c268ec3923aae4fa9afbea49
Faris Durrani <afarisdurrani@gmail.com>
2022-10-30 23:51:13 -0400

    dates enforced

--- main.py
+++ main.py
@@ -1,32 +1,74 @@
 from bs4 import BeautifulSoup
 from decouple import config
+from random import random
 from typing import List, Dict
 import os
 import pandas as pd
 import requests
 
 API_KEY = config("TEST_API_KEY")
-START_DATE = "2015-01-01"
+START_DATES = ["2015-01-01",
+               "2015-03-01",
+               "2015-06-01",
+               "2015-09-01",
+               "2015-12-01",
+
+               "2016-03-01",
+               "2016-06-01",
+               "2016-09-01",
+               "2016-12-01",
+
+               "2017-03-01",
+               "2017-06-01",
+               "2017-09-01",
+               "2017-12-01",
+
+               "2018-03-01",
+               "2018-06-01",
+               "2018-09-01",
+               "2018-12-01",
+
+               "2019-03-01",
+               "2019-06-01",
+               "2019-09-01",
+               "2019-12-01",
+
+               "2020-03-01",
+               "2020-06-01",
+               "2020-09-01",
+               "2020-12-01",
+
+               "2021-03-01",
+               "2021-06-01",
+               "2021-09-01",
+               "2021-12-01",
+
+               "2022-03-01",
+               "2022-06-01",
+               "2022-09-01",
+               ]
 
 
-def abort(contentsToWrite: List[Dict[str, any]], currentPage):
-    writeToCSV(contentsToWrite, f"g-page{currentPage}finalE")
-    raise Exception(f"ERROR: Aborted at page {currentPage}")
+def abort(contentsToWrite: List[Dict[str, any]], filename):
+    writeToCSV(contentsToWrite, filename)
+    raise Exception(f"ERROR: Aborted")
 
 
 def writeToCSV(contentsToWrite: List[Dict[str, any]], filename: str):
     df = pd.DataFrame(contentsToWrite)
-    filepath = f"outputs/{filename}.csv"
+    randomNumber = int(random() * 1e4)
+    filepath = f"outputs/{filename}-{randomNumber}.csv"
     df.to_csv(filepath, encoding='utf-8')
     contentsToWrite.clear()
-    print(f"File successfully {filename} written with "
+    print(f"File successfully {filepath} written with "
           f"size {os.path.getsize(f'{filepath}') / 1e6: _} MB")
 
 
-def queryAPI(currentPage):
+def queryAPI(currentPage, monthListIndex):
     params = {
         "format": "json",
-        "from-date": START_DATE,
+        "from-date": START_DATES[monthListIndex],
+        "to-date": START_DATES[monthListIndex + 1],
         "show-tags": "contributor",
         "show-fields": "headline,short-url,body",
         "api-key": API_KEY,
@@ -44,7 +86,6 @@ def appendResults(results, contentsToWrite):
         contentType = result.get("type", None)
 
         if contentType != "article":
-            print(contentType)
             continue
 
         articleContent = {"timestamp": result.get("webPublicationDate", None),
@@ -72,14 +113,15 @@ def appendResults(results, contentsToWrite):
         contentsToWrite.append(articleContent)
 
 
-def getThisPage(currentPage, contentsToWrite):
-    response = queryAPI(currentPage)
+def getThisPage(currentPage, contentsToWrite, monthListIndex):
+    response = queryAPI(currentPage, monthListIndex)
 
     statusIsOk = (response
                   and response.get("response", None)
                   and response["response"].get("status", None) == "ok")
     if not statusIsOk:
-        abort(contentsToWrite, currentPage)
+        print("response:", response)
+        raise ValueError("Status is not OK")
 
     appendResults(response["response"]["results"], contentsToWrite)
 
@@ -88,22 +130,27 @@ def getThisPage(currentPage, contentsToWrite):
 
 
 def getAllArticles():
+    for monthListIndex in range(len(START_DATES) - 1):
+        collect3Months(monthListIndex)
+
+
+def collect3Months(monthListIndex):
     contentsToWrite = []
     currentPage = 1
     totalPages = currentPage
 
     while currentPage <= totalPages:
-        if currentPage % 10 == 0:
-            writeToCSV(contentsToWrite, f"g-page{currentPage}")
-            return
+        if currentPage % 1000 == 0:
+            writeToCSV(contentsToWrite, f"g-month{monthListIndex}-page{currentPage}")
         print(f"Acquiring page {currentPage} / {totalPages}")
         try:
-            totalPages = getThisPage(currentPage, contentsToWrite)
+            totalPages = getThisPage(currentPage, contentsToWrite,
+                                     monthListIndex)
         except Exception:
-            abort(contentsToWrite, currentPage)
+            abort(contentsToWrite, f"g-month{monthListIndex}-page{currentPage}finalE")
         currentPage += 1
 
-    writeToCSV(contentsToWrite, f"g-page{currentPage}final")
+    writeToCSV(contentsToWrite, f"g-month{monthListIndex}-page{currentPage}final")
 
 
 def main():
