d29ab393066f6bc2890f88c359d6b2dc1ac12602
Faris Durrani <afarisdurrani@gmail.com>
2022-11-02 21:20:39 -0400

    multi  file option in  make_sentiment

--- scripts/make_sentiment.py
+++ scripts/make_sentiment.py
@@ -2,7 +2,10 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import multiprocessing as mp
 
-CSV_FILE_TO_ANALYZE = "g-month3-page625final-2184.csv"
+CSV_FILES = [
+    "g-month10-page429final-9655.csv",
+    "g-month11-page433final-8842.csv"
+]
 ORIGINAL_SAMPLES_DIR = "../outputs/original/"
 SENTIMENTS_DIR = "../outputs/sentiments/"
 analyzer = SentimentIntensityAnalyzer()
@@ -15,14 +18,20 @@ def produce_sentiment(text):
     return analyzer.polarity_scores(text)
 
 
-def run():
-    df = pd.read_csv(ORIGINAL_SAMPLES_DIR + CSV_FILE_TO_ANALYZE)
+def analyze_one_file(filename):
+    df = pd.read_csv(ORIGINAL_SAMPLES_DIR + filename)
 
-    p = mp.Pool(mp.cpu_count() * 3 // 4)
+    p = mp.Pool(mp.cpu_count())
     sentimentColumns = p.map(produce_sentiment, df['bodyContent'])
     df = pd.concat([df, pd.DataFrame(sentimentColumns)], axis=1)
 
-    df.to_csv(f"{SENTIMENTS_DIR}{CSV_FILE_TO_ANALYZE[:-4]}-sentiments.csv")
+    df.to_csv(f"{SENTIMENTS_DIR}{filename[:-4]}-sentiments.csv")
+
+def run():
+    for filename in CSV_FILES:
+        print(f"STARTING: {filename}\n")
+        analyze_one_file(filename)
+        print(f"DONE: {filename}\n")
 
 
 def main():
