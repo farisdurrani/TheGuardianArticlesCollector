3ab84009a4d7c10e52a502b7e14e5508d11049fc
Faris Durrani <afarisdurrani@gmail.com>
2022-11-05 12:59:11 -0400

    Generalized code for make_sentiment

--- scripts/make_sentiment.py
+++ scripts/make_sentiment.py
@@ -3,17 +3,24 @@ import pandas as pd
 import multiprocessing as mp
 
 CSV_FILES = [
-    "g-month10-page429final-9655.csv",
-    "g-month11-page433final-8842.csv"
+    "cnn_articles-2015-6805.csv",
+    "cnn_articles-2016-1610.csv",
+    "cnn_articles-2017-3555.csv",
+    "cnn_articles-2018-7011.csv",
+    "cnn_articles-2019-2682.csv",
+    "cnn_articles-2020-5418.csv",
+    "cnn_articles-2021-8194.csv",
+    "cnn_articles-2022-3264.csv"
 ]
-ORIGINAL_SAMPLES_DIR = "../outputs/original/"
-SENTIMENTS_DIR = "../outputs/sentiments/"
+ORIGINAL_SAMPLES_DIR = "outputs/"
+SENTIMENTS_DIR = "outputs/sentiments/"
+TARGET_COLUMN = "bodyContent"
 analyzer = SentimentIntensityAnalyzer()
 count = [0]
 
 
 def produce_sentiment(text):
-    print(count)
+    print(f"Processing row {count[0]}")
     count[0] += 1
     return analyzer.polarity_scores(text)
 
@@ -22,7 +29,7 @@ def analyze_one_file(filename):
     df = pd.read_csv(ORIGINAL_SAMPLES_DIR + filename)
 
     p = mp.Pool(mp.cpu_count())
-    sentimentColumns = p.map(produce_sentiment, df['bodyContent'])
+    sentimentColumns = p.map(produce_sentiment, df[TARGET_COLUMN])
     df = pd.concat([df, pd.DataFrame(sentimentColumns)], axis=1)
 
     df.to_csv(f"{SENTIMENTS_DIR}{filename[:-4]}-sentiments.csv")
